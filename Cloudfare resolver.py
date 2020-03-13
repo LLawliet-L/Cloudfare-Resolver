7305ecd9dcf7fd4f7ca12c3baa8023b791c8d2ef
LLawliet <49789253+LLawliet-L@users.noreply.github.com>
2020-03-13 22:59:54 +0100

    Add files via upload

---
+++ Cloudfare resolver.py
@@ -0,0 +1,29 @@
+﻿#!/usr/bin/python
+
+import time
+import os
+import sys
+import getpass
+import colorama
+import requests
+from colorama import Back, Fore, Style, deinit, init
+init()
+
+print(Fore.RED)
+
+
+print(" ██████╗███████╗    ██████╗ ███████╗███████╗ ██████╗ ██╗    ██╗   ██╗███████╗██████╗ ")
+print("██╔════╝██╔════╝    ██╔══██╗██╔════╝██╔════╝██╔═══██╗██║    ██║   ██║██╔════╝██╔══██╗")
+print("██║     █████╗      ██████╔╝█████╗  ███████╗██║   ██║██║    ██║   ██║█████╗  ██████╔╝")
+print("██║     ██╔══╝      ██╔══██╗██╔══╝  ╚════██║██║   ██║██║    ╚██╗ ██╔╝██╔══╝  ██╔══██╗")
+print("╚██████╗██║         ██║  ██║███████╗███████║╚██████╔╝███████╗╚████╔╝ ███████╗██║  ██║")
+print(" ╚═════╝╚═╝         ╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝ ╚══════╝ ╚═══╝  ╚══════╝╚═╝  ╚═╝")                                                                                 
+print(Fore.GREEN)
+print("                    --Cloudfare resolver-- [BY LAWLI3T]    ")
+print(Fore.MAGENTA)
+     
+website=input('\bEnter website : ')
+r = requests.get('https://webresolver.nl/api.php?key=5HUPH-I6VEP-TXBGM-3HL8L&action=cloudflare&string=' + website)
+print(Fore.CYAN + "\nResult : ")
+print(Fore.YELLOW)
+print(r.text)      
